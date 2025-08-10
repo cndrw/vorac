d5f81e3ec9baa9b5f9a7ceb00d6d935993bb2051
Conrad <122440196+cndrw@users.noreply.github.com>
2025-08-10 19:37:27 +0200

    made terminal output better

--- prototype/test_model.py
+++ prototype/test_model.py
@@ -21,20 +21,35 @@ with open(ROOT / "models" / "models.pkl", "rb") as f:
 
 files = sorted(set([file.stem for file in validation_dir.iterdir()]))[:-2] # exlude output dir and .trans file
 
-PHONEMES = ['n', 's', 'p']   
+PHONEMES = ['n', 's', 'p']
 
-all_tests = []
-for file in files:
-    mfccs = vru.generate_mfccs_from_textgrid(file=file, data_dir=validation_dir, phoneme=u"p")
-    all_tests.append(mfccs)
+tests = { p: [] for p in PHONEMES }
+for phoneme in PHONEMES:
+    for file in files:
+        mfccs = vru.generate_mfccs_from_textgrid(file=file, data_dir=validation_dir, phoneme=phoneme)
+        tests[phoneme].append(mfccs)
 
-total_test = sum(len(mfcc) for mfcc in all_tests)
-print(f"Testing on {len(all_tests)} files with {total_test} occurences...")
+# tests[phoneme][file][phoneme occurrence] = mfcc
 
-count = 0
-for i, mfcc in enumerate(all_tests):
-    for j, segment in enumerate(mfcc):
-        scores = { phoneme: model.score(segment) for phoneme, model in models.items() }
-        best_phoneme = max(scores, key=scores.get)
-        count += 1
-        print(f"Best fit [{count:03}/{total_test}]: {best_phoneme}")
+total_test = sum(len(mfcc) for mfccs in tests.values() for mfcc in mfccs)
+print(f"Testing with {len(files)} files")
+
+fails = []
+# Testing '$phoneme' [fileIdx/files][currTestIdx/allPhonemesOccurences] : $checkmark
+for phoneme, all_mfccs in tests.items():
+    res = []
+    for i, mfccs in enumerate(all_mfccs):
+        for j, segment in enumerate(mfccs):
+            scores = { phoneme: model.score(segment) for phoneme, model in models.items() }
+            best_phoneme = max(scores, key=scores.get)
+
+            checkmark = '✔️' if best_phoneme == phoneme else '❌'
+            res.append(checkmark)
+            if checkmark == '❌':
+                fails.append((phoneme, i, j, best_phoneme))
+    print(f"Testing '{phoneme}' : {'️️✔️' if all(checkmark == '✔️' for checkmark in res) else '❌'}")
+
+
+print("-" * 30)
+for phoneme, i, j, best_phoneme in fails:
+    print(f"Failed: Testing '{phoneme}' [{(i+1):02}/{len(tests[phoneme]):02}][{j+1:02}/{len(tests[phoneme][i]):02}] : ❌")
--- prototype/train_model.py
+++ prototype/train_model.py
@@ -14,13 +14,15 @@ def extract_data(data_dir: Path, phoneme: str) -> list[np.ndarray]:
     files = sorted(set([file.stem for file in data_dir.iterdir()]))[:-2] # exlude output dir and .trans file
 
     total_files = len(files)
-    count = 0
-    
-    for file in files:
+    ending = '\r'
+
+    for i, file in enumerate(files):
         mfccs = vru.generate_mfccs_from_textgrid(file, data_dir, phoneme)
         all_mfccs += mfccs
-        count += 1
-        print(f"File {count}/{total_files} processed for '{phoneme}'")
+
+        if i == total_files - 1:
+            ending = '\n'
+        print(f"File {i + 1}/{total_files} processed for '{phoneme}'", end=ending)
 
     return all_mfccs
 
@@ -38,7 +40,22 @@ def train_models(data_dir: Path, phoneme: list[str]) -> dict[str, hmm.GaussianHM
 training_data = ROOT_DIR / "data" / "198"
 
 # list with all phonemes
-PHONEMES = ['n', 's', 'p']
+# PHONEMES = ['n', 's', 'p']
+
+# PHONEMES = [
+#     # Consonants
+#     u"p", u"b", u"t", u"d", u"k", u"g",
+#     u"tʃ", u"dʒ", u"f", u"v", u"θ", u"ð",
+#     u"s", u"z", u"ʃ", u"ʒ", u"h", u"m",
+#     u"n", u"ŋ", u"l", u"r", u"j", u"w",
+
+#     # Vowels (monophthongs)
+#     u"i", u"ɪ", u"e", u"ɛ", u"æ",
+#     u"ɑ", u"ɒ", u"ɔ", u"ʌ", u"ʊ", u"u", u"ɝ", u"ɚ", u"ə",
+
+#     # Diphthongs
+#     u"aɪ", u"aʊ", u"ɔɪ", u"eɪ", u"oʊ"
+# ]
 
 models = train_models(training_data, PHONEMES)
 
