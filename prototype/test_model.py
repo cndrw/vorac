558d300f2ea8e01ddbd12686d0d9a40847825d1b
Conrad <122440196+cndrw@users.noreply.github.com>
2025-08-12 19:04:56 +0200

    added a proper list of phonemes
    now a model for every phoneme can be trained and tested

--- prototype/test_model.py
+++ prototype/test_model.py
@@ -21,35 +21,22 @@ with open(ROOT / "models" / "models.pkl", "rb") as f:
 
 files = sorted(set([file.stem for file in validation_dir.iterdir()]))[:-2] # exlude output dir and .trans file
 
-PHONEMES = ['n', 's', 'p']
-
-tests = { p: [] for p in PHONEMES }
-for phoneme in PHONEMES:
+phonemes = list(models.keys())
+tests = { p: [] for p in phonemes }
+for phoneme in phonemes:
     for file in files:
         mfccs = vru.generate_mfccs_from_textgrid(file=file, data_dir=validation_dir, phoneme=phoneme)
-        tests[phoneme].append(mfccs)
+        tests[phoneme] += mfccs
 
-# tests[phoneme][file][phoneme occurrence] = mfcc
-
-total_test = sum(len(mfcc) for mfccs in tests.values() for mfcc in mfccs)
 print(f"Testing with {len(files)} files")
 
-fails = []
-# Testing '$phoneme' [fileIdx/files][currTestIdx/allPhonemesOccurences] : $checkmark
-for phoneme, all_mfccs in tests.items():
-    res = []
-    for i, mfccs in enumerate(all_mfccs):
-        for j, segment in enumerate(mfccs):
-            scores = { phoneme: model.score(segment) for phoneme, model in models.items() }
-            best_phoneme = max(scores, key=scores.get)
+for phoneme, mfccs in tests.items():
+    passed_counter = 0
+    for mfcc in mfccs:
+        scores = { phoneme: model.score(mfcc) for phoneme, model in models.items() }
+        best_phoneme = max(scores, key=scores.get)
 
-            checkmark = '✔️' if best_phoneme == phoneme else '❌'
-            res.append(checkmark)
-            if checkmark == '❌':
-                fails.append((phoneme, i, j, best_phoneme))
-    print(f"Testing '{phoneme}' : {'️️✔️' if all(checkmark == '✔️' for checkmark in res) else '❌'}")
+        passed_counter += int(best_phoneme == phoneme)
 
+    print(f"Testing '{phoneme}': Passed {passed_counter}/{len(mfccs)} {'️️✔️' if passed_counter > (len(mfccs) // 2) else '❌'}")
 
-print("-" * 30)
-for phoneme, i, j, best_phoneme in fails:
-    print(f"Failed: Testing '{phoneme}' [{(i+1):02}/{len(tests[phoneme]):02}][{j+1:02}/{len(tests[phoneme][i]):02}] : ❌")
--- prototype/train_model.py
+++ prototype/train_model.py
@@ -22,12 +22,20 @@ def extract_data(data_dir: Path, phoneme: str) -> list[np.ndarray]:
 
         if i == total_files - 1:
             ending = '\n'
-        print(f"File {i + 1}/{total_files} processed for '{phoneme}'", end=ending)
+        print(f"File {i + 1}/{total_files} processed for '{phoneme}' ({len(all_mfccs)} samples)", end=ending)
 
     return all_mfccs
 
 def train_models(data_dir: Path, phoneme: list[str]) -> dict[str, hmm.GaussianHMM]:
     data = { p: extract_data(data_dir, p) for p in phoneme }
+
+    # data = { p: mfccs for p, mfccs in data.items() if len(mfccs) > 0 }
+
+    min_samples = 20
+    for key in list(data.keys()):
+        if len(data[key]) < 20:
+            del data[key]
+            print(f"Removed phoneme '{key}' due to less than {min_samples} data samples.")
 
     models = {}
     for phoneme, mfccs in data.items():
@@ -39,23 +47,17 @@ def train_models(data_dir: Path, phoneme: list[str]) -> dict[str, hmm.GaussianHM
 
 training_data = ROOT_DIR / "data" / "198"
 
-# list with all phonemes
-# PHONEMES = ['n', 's', 'p']
+PHONEMES = [
+    u"", u"aj", u"aw", u"b", u"bʲ", u"c", u"cʰ", u"cʷ", u"d", u"dʒ", u"dʲ",
+    u"d̪", u"ej", u"f", u"fʲ", u"h", u"i", u"iː", u"j", u"k", u"kʰ",
+    u"kʷ", u"l", u"m", u"mʲ", u"m̩", u"n", u"n̩", u"ow", u"p", u"pʰ",
+    u"pʲ", u"pʷ", u"s", u"t", u"tʃ", u"tʰ", u"tʲ", u"tʷ", u"t̪", u"v",
+    u"vʲ", u"w", u"z", u"æ", u"ç", u"ð", u"ŋ", u"ɐ", u"ɑ", u"ɑː",
+    u"ɒ", u"ɒː", u"ɔj", u"ə", u"ɚ", u"ɛ", u"ɝ", u"ɟ", u"ɟʷ", u"ɡ",
+    u"ɡʷ", u"ɪ", u"ɫ", u"ɫ̩", u"ɱ", u"ɲ", u"ɹ", u"ɾ", u"ɾʲ", u"ɾ̃",
+    u"ʃ", u"ʉ", u"ʉː", u"ʊ", u"ʎ", u"ʒ", u"ʔ", u"θ"
+]
 
-# PHONEMES = [
-#     # Consonants
-#     u"p", u"b", u"t", u"d", u"k", u"g",
-#     u"tʃ", u"dʒ", u"f", u"v", u"θ", u"ð",
-#     u"s", u"z", u"ʃ", u"ʒ", u"h", u"m",
-#     u"n", u"ŋ", u"l", u"r", u"j", u"w",
-
-#     # Vowels (monophthongs)
-#     u"i", u"ɪ", u"e", u"ɛ", u"æ",
-#     u"ɑ", u"ɒ", u"ɔ", u"ʌ", u"ʊ", u"u", u"ɝ", u"ɚ", u"ə",
-
-#     # Diphthongs
-#     u"aɪ", u"aʊ", u"ɔɪ", u"eɪ", u"oʊ"
-# ]
 
 models = train_models(training_data, PHONEMES)
 
